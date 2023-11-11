9dfb91421f9d195475e5519985bb49e81af7ca31
Martin <david.martin6@ascension.org>
2023-11-10 21:41:14 -0600

    added initial store files and types

---
+++ src/store/OrderTasks/orderTasks.types.ts
@@ -0,0 +1,67 @@
+
+
+
+export type Provider = {
+    id: number;
+    firstName: string;
+    lastName: string;
+    NPI: string;
+    Specialty: string;
+}
+
+export type User = {
+    userId: number;
+    userName: string;
+}
+
+
+export type ActionNote = {
+    user: User;
+    timeStamp: Date;
+    data: string;
+}
+
+export type Department = {
+    departmentName: string;
+    departmentId: number;
+}
+
+export type Patient = {
+    firstName: string;
+    lastName: string;
+    athenaId: number;
+}
+
+
+export type Order = {
+    orderNumber: number;
+    orderDate: Date;
+    orderProvider: Provider;
+    carrier: string;
+    authStatus: 'Not Started' | 'Pending' | 'Auth Obtained' | 'Auth Denied' | 'Pending P2P';
+    scheduleStatus: 'Not Scheduled' | 'Scheduled' | 'Scheduled Outside Facility';
+    priority: boolean;
+    department: Department;
+    patient: Patient;
+    assignedUser: User;
+    lastUpdated:Date;
+    notes: ActionNote[]
+}
+
+
+
+export type Metric = {
+    name: 'Not Started' | 'Pending' | 'Completed';
+    count: number
+}
+
+export type Group = {
+    groupId: number;
+    provider: Provider;
+    user: User;
+    department:Department;
+    notStarted: Metric;
+    pending: Metric;
+    completed: Metric;
+
+}
---
+++ src/store/store.ts
@@ -0,0 +1,19 @@
+import {compose, createStore, applyMiddleware} from 'redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import { AnyAction } from 'redux';
+import { Dispatch } from 'redux';
+import { ThunkDispatch } from 'redux-thunk';
+
+import { rootReducer } from './rootreducer';
+
+
+
+const middleWares = [logger, thunk];
+
+const composedEnhancers = compose(applyMiddleware(...middleWares))
+
+export const store = createStore(rootReducer,undefined,composedEnhancers)
+
+export type RootState = ReturnType<typeof rootReducer>
+export type AppDispatch = Dispatch<AnyAction> & ThunkDispatch<RootState, null, AnyAction> 
