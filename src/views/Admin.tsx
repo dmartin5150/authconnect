78491a9332c1c102a32e29ee1d0b9c1d595725b9
Martin <david.martin6@ascension.org>
2023-11-13 19:57:03 -0600

    laid out admin page

--- src/views/Admin.tsx
+++ src/views/Admin.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import DualListBox from 'react-dual-listbox';
+import GroupDropdown from '../components/GroupDropdown';
 import 'react-dual-listbox/lib/react-dual-listbox.css';
 import './Admin.css'
 
@@ -15,15 +16,33 @@ function Admin() {
 
     return (
         <div className='admin'>
-            <div className='admin-dualbox'>
-                <DualListBox
-                    canFilter
-                    lang={{availableHeader: 'Available Department', selectedHeader:'Selected Departments'}}
-                    options={options}
-                    selected={selected}
-                    onChange={(value:string[]) => setSelected(value)}
-                    showHeaderLabels={true}
-                />
+            <div className='admin-controls'>
+                <div className='admin-controls-groups'>
+                    <div className='admin-controls-groupdropdown'>
+                        <label>Select Group</label>
+                        <GroupDropdown />
+                    </div>
+                    <div className='admin-controls-buttons'>
+                        <div className='admin-controls-buttons-section'>
+                            <button className='admin-controls-buttons right'>Select Department</button>
+                            <button className='admin-controls-buttons'>Select Users</button>
+                        </div>
+                        <div className='admin-controls-buttons-section'>
+                            <button className='admin-controls-buttons right'>Add New Group</button>
+                            <button className='admin-controls-buttons'>Select Providers</button>
+                        </div>
+                    </div>
+                </div>
+                <div className='admin-dualbox'>
+                    <DualListBox
+                        // canFilter
+                        lang={{availableHeader: 'Available Department', selectedHeader:'Selected Departments'}}
+                        options={options}
+                        selected={selected}
+                        onChange={(value:string[]) => setSelected(value)}
+                        showHeaderLabels={true}
+                    />
+                </div>
             </div>
             <div className='admin-grouplist'>
                 <GroupList />
